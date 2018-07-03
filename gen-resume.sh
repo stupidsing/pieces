2fda6b61b2e4b8d64ae5bafd3421ef534d21b2ea
stupidsing <stupidsing@gmail.com>
2018-07-03 21:29:49 +0800

    -

--- gen-resume.sh
+++ gen-resume.sh
@@ -1,3 +1,4 @@
-cat resume.html | sed s/_CS_/40/g | sed s/_ES_/50/g > /tmp/resume.html &&
+DIR=$(dirname "${0}")
+cat ${DIR}/resume.html | sed s/_CS_/40/g | sed s/_ES_/50/g > /tmp/resume.html &&
 wkhtmltopdf --encoding UTF-8 --minimum-font-size 36 -d 100 -s A4 /tmp/resume.html /tmp/resume.pdf &&
 scp /tmp/resume.{html,pdf} stupidsing.no-ip.org:public_html/
