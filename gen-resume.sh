44190beb949e33083261f7b73e366acf64b2fa35
ywsing <stupidsing@gmail.com>
2018-09-12 15:19:56 +0800

    -

--- gen-resume.sh
+++ gen-resume.sh
@@ -1,5 +1,5 @@
 DIR=$(dirname "${0}")
-cat ${DIR}/resume.html | sed s/_CS_/40/g | sed s/_ES_/48/g > /tmp/resume-ywsing.html &&
+cat ${DIR}/resume.html | sed s/_CS_/48/g | sed s/_ES_/64/g > /tmp/resume-ywsing.html &&
 wkhtmltopdf --encoding UTF-8 --minimum-font-size 36 -d 100 -s A4 /tmp/resume-ywsing.html /tmp/resume-ywsing.pdf &&
 scp /tmp/resume-ywsing.{html,pdf} stupidsing.no-ip.org:public_html/
 scp /tmp/resume-ywsing.{html,pdf} ywsing.onedse.com:.
