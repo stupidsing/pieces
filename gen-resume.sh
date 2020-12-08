57fec1912e6e0c0c8193f0afbd53b55e54f5ed6f
ywsing <stupidsing@gmail.com>
2020-12-08 21:49:00 +0800

    -

--- gen-resume.sh
+++ gen-resume.sh
@@ -1,6 +1,6 @@
 DIR=$(dirname "${0}")
 cat ${DIR}/resume.html | sed s/_CS_/48/g | sed s/_ES_/64/g > /tmp/resume-ywsing.html &&
-wkhtmltopdf --encoding UTF-8 --minimum-font-size 36 -d 100 -s A4 /tmp/resume-ywsing.html /tmp/resume-ywsing.pdf &&
+wkhtmltopdf --encoding UTF-8 --minimum-font-size 18 -d 100 -s A4 /tmp/resume-ywsing.html /tmp/resume-ywsing.pdf &&
 scp /tmp/resume-ywsing.{html,pdf} stupidsing.no-ip.org:public_html/
 scp /tmp/resume-ywsing.{html,pdf} pointless.online:.
 echo ssh pointless.online sudo mv resume-ywsing.pdf /var/www/html/
